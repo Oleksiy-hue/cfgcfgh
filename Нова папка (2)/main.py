49f19460910c70f3d81fa1614bf9c4bbe8c511d1
Oleksiy-hue <xalamenos@gmail.com>
2024-11-30 14:50:13 +0200

    Add files via upload

---
+++ Нова папка (2)/main.py
@@ -0,0 +1,41 @@
+# number=10
+# print(number)
+# wrong_number=10.23
+# print(wrong_number)
+# text="Hello my little friend"
+# print(text)
+# tru=True
+# print(tru)
+
+
+# x=6
+# y=2
+# b=4
+# c=9
+# d=x+b*c-y
+# if 20>d>10:
+#     print("True")
+# elif d<10:
+#     print("False")
+# elif d==10:
+#     print("eshkere")
+#     if d == 9:
+#         print("d дорівнює 9 це не 10 тому переробляй")
+#     else:
+#         print("sigma")
+#     if d > 50:
+#         print("мені не подобається")
+#     else:
+#         print("вже краще")
+
+
+
+for i in range(50):
+    print("Hello, World!")
+    
+
+i = 0
+while i < 50:
+    print("Hello, World!")
+    i += 1
+  
